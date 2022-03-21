6e031aaa1e8cfa7f3d9bb3adcabe25c1d12ecd05
Cameron DeCoster <cameron.decoster@gmail.com>
2022-03-21 08:37:39 -0600

    Updates Bootstrap theme; adds main element

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue';
 import BootstrapVue from '@coreui/bootstrap-vue';
 
 // Import styles
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootswatch/dist/slate/bootstrap.min.css';
 
 import App from './App.vue';
 
